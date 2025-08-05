99a7d7858427691011b81a5c2f663d119f129bfb
lakmal1992 <74525369+lakmal1992@users.noreply.github.com>
2025-08-05 16:58:00 +0700

    Config

--- vite.config.js
+++ vite.config.js
@@ -4,31 +4,31 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  assetsInclude: ["**/*.ttf"],
+  optimizeDeps: {
+    include: ["jspdf", "jspdf-autotable"]
+  },
   server: {
-    host: "0.0.0.0", // This will allow you to access the server from another device
+    host: "0.0.0.0",
     port: 3001,
     open: true,
-    assetsInclude: ["**/*.ttf"],
-    build: {
-      minify: false,
-      sourcemap: true,
-      chunkSizeWarningLimit: 20000,
-      rollupOptions: {
-        manualChunks: {
-          "jspdf-autotable": ["jspdf-autotable"]
-        }
-      }
-    },
     https: {
       key: fs.readFileSync(
-        //"/Users/dilanlakmal/Downloads/YQMS-Latest-main/192.165.2.175-key.pem"
-
         "C:/Users/USER/Downloads/YQMS-V0.1-main/YQMS-V0.1-main/192.167.12.162-key.pem"
       ),
       cert: fs.readFileSync(
-        //"/Users/dilanlakmal/Downloads/YQMS-Latest-main/192.165.2.175.pem"
         "C:/Users/USER/Downloads/YQMS-V0.1-main/YQMS-V0.1-main/192.167.12.162.pem"
       )
+    }
+  },
+  build: {
+    minify: false, // Use esbuild for minification
+    sourcemap: true,
+    chunkSizeWarningLimit: 20000,
+    rollupOptions: {
+      manualChunks: {
+        "jspdf-autotable": ["jspdf-autotable"]
+      }
     }
   }
 });
