7a7bb3912f2781bf3076e18927f47b66bcbafae9
lakmal1992 <74525369+lakmal1992@users.noreply.github.com>
2026-01-09 16:40:14 +0700

    Config changes

--- backend/Config/appConfig.js
+++ backend/Config/appConfig.js
@@ -20,8 +20,7 @@ export const __dirname = path.dirname(__filename);
 // Define a base directory for the backend root
 export const __backendDir = path.resolve(__dirname, "..");
 
-export const API_BASE_URL =
-  process.env.API_BASE_URL || "https://192.167.6.207:5001";
+export const API_BASE_URL = process.env.API_BASE_URL;
 
 const options = {
   key: fs.readFileSync(
