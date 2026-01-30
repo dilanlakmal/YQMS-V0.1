9f594c892973878450bdd63a6ae51e25ab7c4364
YashPS24 <psyasomi@gmail.com>
2026-01-30 14:37:10 +0700

    Fix th humidity issue

--- src/components/inspection/Humidity/HistoryModal.jsx
+++ src/components/inspection/Humidity/HistoryModal.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import { Modal, Image } from "antd";
 import { renderToStaticMarkup } from "react-dom/server";
 import PaperPreview from "./PaperPreview";
